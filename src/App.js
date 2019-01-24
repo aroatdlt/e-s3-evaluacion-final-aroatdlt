2e3bdd44e4465fa54195c36cca74a7c861899dd8
aroatdlt <aroatato@gmx.com>
2019-01-24 17:54:04 +0100

    Good function to filter and function to change first letters

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ class App extends Component {
     this.state = {
       infoRawPotter: [],
       loading: true,
+      query: "",
+      filter: [],
     }
+    this.searchedInfo = this.searchedInfo.bind(this);
+    this.filterResults = this.filterResults.bind(this);
   }
 
   componentDidMount() {
@@ -26,12 +30,30 @@ class App extends Component {
         this.setState({
           infoRawPotter: newDataWithId,
           loading: false,
-        })
-      })
+        });
+      });
+  }
+
+  searchedInfo(event){
+    const inputValue = event.currentTarget.value;
+    const finalInputValue = inputValue.toLowerCase()
+    .split(' ')
+    .map((s)=> s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+    this.setState({
+      query: finalInputValue
+    })
+  }
+
+  filterResults(){
+    const filteredResults = this.state.infoRawPotter.filter(character => {
+      return((character.name.includes(this.state.query))? true : false
+    )})
+    return filteredResults;
   }
 
   render() {
-    const { infoRawPotter } = this.state;
+    const filterCharacter = this.filterResults();
     return (
       <div className="App">
         <header className="header">
@@ -39,12 +61,12 @@ class App extends Component {
         </header>
         <main className="main">
           <label htmlFor="character" />
-          <input type="text" id="character" placeholder="Escribe el nombre de tu personaje favorito"></input>
+          <input type="text" id="character" placeholder="Escribe el nombre de tu personaje favorito" onKeyUp={this.searchedInfo}></input>
           {this.state.loading ? (
             <Loader />
           ) : (
               <ul className="list__characters">
-                {infoRawPotter.map(character => {
+                {filterCharacter.map(character => {
                   return <li key={character.id} className="character">
                     <img src={character.image} alt={character.name} />
                     <h2 className="character__name">{character.name}</h2>
